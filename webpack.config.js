59245c1a78ba9b5e1f8b6e226e1254c52729b549
Alexander Fisher <afisher@alexander-fisher.com>
2017-02-04 12:47:44 -0500

    Use ts loader instead of awesome typescript loader. Git client building.

--- webpack.config.js
+++ webpack.config.js
@@ -6,17 +6,17 @@ module.exports = {
   },
   devtool: "source-map",
   resolve: {
-    extensions: [".tsx"]
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
   },
   module: {
     loaders: [{
       test: /\.tsx?$/,
-      loader: "awesome-typescript-loader"
-    }],
+      loader: "ts-loader"
+    }]/*,
     preLoaders: [{
       test: /\.js$/,
       loader: "source-map-loader"
-    }]
+    }]*/
   },
   externals: {
     "react": "React",
