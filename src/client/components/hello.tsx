f32e2519c4e38a739e3b371ae48a8fe33cea026b
Alexander Fisher <afisher@alexander-fisher.com>
2017-02-04 16:20:29 -0500

    Use class inheritance for Hello component.

--- src/client/components/hello.tsx
+++ src/client/components/hello.tsx
@@ -5,6 +5,8 @@ export interface HelloProps {
   framework: string;
 }
 
-export const Hello = (props: HelloProps) => {
-  return <h1>Hello from {props.compiler} and {props.framework}!</h1>
+export class Hello extends React.Component<HelloProps, {}> {
+  render() {
+    return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+  }
 }
